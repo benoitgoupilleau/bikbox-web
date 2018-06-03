7bee395530681df824d2eb69917bb9a72d1bbc98
benoitgoupilleau <benoit.goupilleau@gmail.com>
2018-06-03 21:29:49 +0200

    updated axios

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.API_URL,
   timeout: 1000,
-  headers: {'x-key': process.env.X_KEY}
+  headers: { 'x-key': process.env.X_KEY, 'Access-Control-Allow-Origin': '*' }
 });
 
 export default instance;
