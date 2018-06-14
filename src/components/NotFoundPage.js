d790bb7b1593a019d837c5eb7d4b8685868ab530
benoitgoupilleau <benoit.goupilleau@gmail.com>
2018-06-14 20:30:10 +0200

    Updated Notfound page to display specific message

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -27,20 +27,38 @@ const StyledLink = styled(Link)`
 `;
 
 
-const NotFoundPage = () => (
-  <Background >
-    <BoxLayout>
-      <BoxTitle>
-        <img
-          src="https://static.wixstatic.com/media/078127_6d46d897f14149ab9de07f4a1b1295c7~mv2.png/v1/crop/x_62,y_139,w_988,h_366/fill/w_480,h_164,al_c,usm_0.66_1.00_0.01/078127_6d46d897f14149ab9de07f4a1b1295c7~mv2.png"
-          alt="Bik'box"
-          width="100%"
-        />
-      </BoxTitle>
-      <p>404 - Introuvable</p>
-      <StyledLink to="/" >Retour</StyledLink>
-    </BoxLayout>
-  </Background>
-);
+const NotFoundPage = (props) => {
+  let message = 'Introuvable';
+  if (!!props.location.hash) {
+    switch (props.location.hash) {
+      case '#nouser':
+        message = 'Aucun utilisateur trouvé'
+        break;
+      case '#expired':
+        message = 'Le token a expiré'
+        break;
+      case '#badrequest':
+        message = 'Mauvaise requête'
+        break;
+      default:
+        break;
+    }
+  }
+  return (
+    <Background >
+      <BoxLayout>
+        <BoxTitle>
+          <img
+            src="https://static.wixstatic.com/media/078127_6d46d897f14149ab9de07f4a1b1295c7~mv2.png/v1/crop/x_62,y_139,w_988,h_366/fill/w_480,h_164,al_c,usm_0.66_1.00_0.01/078127_6d46d897f14149ab9de07f4a1b1295c7~mv2.png"
+            alt="Bik'box"
+            width="100%"
+          />
+        </BoxTitle>
+        <p>404 - {message}</p>
+        <StyledLink to="/" >Retour</StyledLink>
+      </BoxLayout>
+    </Background>
+    );
+}
 
 export default NotFoundPage;
